95c05bf7e8787c7e0a92b44b243fee53c351a48b
Alexey Kuznetsov <akuznetzz@yandex.ru>
2022-07-14 11:48:08 +0300

    факториал

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,18 @@
 function factorial(n) {
-  // ваш код...
+  
+  //let result;
+
+  let result = 1;
+  
+  for (let i = n; i > 1; i-- ) {
+    
+    if (i === 0) {
+      
+      break;
+    
+    } else result *= i;
+
+    // result = (i === 0) ? 1 : result *= i;
+
+  } return result;
 }
