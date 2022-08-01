c8a49205d705a7214177f5c2ad8a1d4b33fd3968
Alexey Kuznetsov <akuznetzz@yandex.ru>
2022-08-01 14:05:30 +0300

    3 задача через делегирование

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,39 +1,33 @@
 function initCarousel() {
   let rightArrow = document.querySelector('.carousel__arrow_right');
   let leftArrow = document.querySelector('.carousel__arrow_left');
+  let carousel = document.querySelector('.carousel');
   let carouselInner = document.querySelector('.carousel__inner');
   let position = 0;
   leftArrow.style.display = 'none';
   
-  rightArrow.addEventListener('click', () => {
+  carousel.addEventListener('click', (event) => {
     
-    if (position < (3 * carouselInner.offsetWidth)) {
-      
+    if (event.target.closest('.carousel__arrow').classList.contains("carousel__arrow_right")) {
+      if (position < (3 * carouselInner.offsetWidth)) {
       position += carouselInner.offsetWidth;
-      
       carouselInner.style.transform = `translateX(-${position}px)`;
-      
       leftArrow.style.display = '';
-      
+      }; 
+    
+      if (position == (3 * carouselInner.offsetWidth)) {rightArrow.style.display = 'none'};
     };
-   
-    if (position == (3 * carouselInner.offsetWidth)) {rightArrow.style.display = 'none'};
- 
-  })
 
-  leftArrow.addEventListener('click', () => {
-    
-    if (position > 0) {
-        
+    if (event.target.closest('.carousel__arrow').classList.contains("carousel__arrow_left")) {
+      if (position > 0) {
       position -= carouselInner.offsetWidth;
-        
-      carouselInner.style.transform = `translateX(-${position}px)`;
-        
+      carouselInner.style.transform = `translateX(-${a}px)`;
       rightArrow.style.display = '';
+      };
       
+      if (position === 0) {leftArrow.style.display = 'none'};
     };
-    
-    if (position == 0) {leftArrow.style.display = 'none'};
-})
+
+  });
 
 }
