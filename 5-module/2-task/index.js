bdabdc0cf7517a86492ae5b40f3e1805bd9251af
Alexey Kuznetsov <akuznetzz@yandex.ru>
2022-08-01 13:37:38 +0300

    Задачи 5 модуля

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
-}
+  
+  let button = document.querySelector('.toggle-text-button'); 
+  
+  button.addEventListener('click', () => {
+     
+    if (!text.hidden) {
+      
+      text.hidden = true;
+     
+    } else text.hidden = false;
+  });
+};
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,39 @@
 function initCarousel() {
-  // ваш код...
+  let rightArrow = document.querySelector('.carousel__arrow_right');
+  let leftArrow = document.querySelector('.carousel__arrow_left');
+  let carouselInner = document.querySelector('.carousel__inner');
+  let position = 0;
+  leftArrow.style.display = 'none';
+  
+  rightArrow.addEventListener('click', () => {
+    
+    if (position < (3 * carouselInner.offsetWidth)) {
+      
+      position += carouselInner.offsetWidth;
+      
+      carouselInner.style.transform = `translateX(-${position}px)`;
+      
+      leftArrow.style.display = '';
+      
+    };
+   
+    if (position == (3 * carouselInner.offsetWidth)) {rightArrow.style.display = 'none'};
+ 
+  })
+
+  leftArrow.addEventListener('click', () => {
+    
+    if (position > 0) {
+        
+      position -= carouselInner.offsetWidth;
+        
+      carouselInner.style.transform = `translateX(-${position}px)`;
+        
+      rightArrow.style.display = '';
+      
+    };
+    
+    if (position == 0) {leftArrow.style.display = 'none'};
+})
+
 }
