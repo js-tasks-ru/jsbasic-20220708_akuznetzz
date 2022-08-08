889dca28f3905443c3b7d56e87fb4b9fde855244
Alexey Kuznetsov <akuznetzz@yandex.ru>
2022-08-08 16:51:24 +0300

    задачи 6 модуля

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -14,5 +14,60 @@
  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.render();
+    this.elem.addEventListener('click', (event) => {           
+      if(event.target.tagName === 'BUTTON') {
+        event.target.parentElement.remove()
+      } 
+    });
   }
+
+
+  render() {
+          /* заголовки таблицы */
+          let headers = ['Имя', 'возраст', 'зарплата', 'город']
+      
+          let table = document.createElement('table');
+          document.body.append(table)
+    
+          let header = document.createElement('thead')
+          table.append(header)
+    
+          let headerRow = document.createElement('tr')
+          header.append(headerRow)
+    
+          headerRow.innerHTML = headers.map(item =>`<th>${item}</th>`).join('')
+          
+            /* тело таблицы */
+          let tableBody = document.createElement('tbody')
+          table.append(tableBody)
+          
+          this.rows.map(item => {
+            let tableRow = document.createElement('tr')
+            let deleteButton = document.createElement('button')
+            deleteButton.textContent = 'X'
+            for (let prop in item) {
+              let td = document.createElement('td') 
+              td.textContent = item[prop]
+              tableRow.append(td)
+            }
+            
+            tableRow.append(deleteButton)
+            tableBody.append(tableRow)
+          })
+          
+          
+
+          
+    
+          
+
+          return table
+
+  }
+
+
+
+  
 }
